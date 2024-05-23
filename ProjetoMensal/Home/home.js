046f98ecbccbb8b14153ee9bdc1943dd16950295
PedroMajewski <pedrohmssilva@gmail.com>
2024-05-23 17:25:22 -0300

    commit-mensal

--- ProjetoMensal/Home/home.js
+++ ProjetoMensal/Home/home.js
@@ -1,7 +1,17 @@
 //Transição Menu mobile - responsividade
-let btn_menu =  document.querySelector('.btn-menu')
-let menu_dropdown = document.getElementsByClassName('dropside-mobile')
+var btn_icone = document.getElementById("btn-menu");
+var menu = document.getElementById("dropside-mobile");
 
-btn_menu.addEventListener('click', ()=>{
-    menu_dropdown.classList.add('aberto')
-});
+document.getElementById("btn-menu").addEventListener("click", Menu_mobile);
+
+function Menu_mobile(){
+    var menu = document.getElementById("dropside-mobile");
+    menu.classList.add("aberto");
+}
+
+// let btn_menu =  document.querySelector('.btn-menu')
+// let menu_dropdown = document.getElementsByClassName('dropside-mobile')
+
+// btn_menu.addEventListener('click', ()=>{
+//     menu_dropdown.classList.add('aberto')
+// });
