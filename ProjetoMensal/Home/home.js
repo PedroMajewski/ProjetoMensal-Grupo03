93602bc354a76f16b1ab4c6c7aca706f598e23a3
PedroMajewski <pedrohmssilva@gmail.com>
2024-05-23 18:01:05 -0300

    commit-servicos

--- ProjetoMensal/Home/home.js
+++ ProjetoMensal/Home/home.js
@@ -1,14 +1,39 @@
 //Transição Menu mobile - responsividade
-var btn_icone = document.getElementById("btn-menu");
-var menu = document.getElementById("dropside-mobile");
+let btn_menu = document.getElementById("btn-menu");
+let menu_lateral = document.querySelector(".dropside-mobile");
 
-document.getElementById("btn-menu").addEventListener("click", Menu_mobile);
+let imagem_nova = "../Assets/Menu.svg";
+let imagem_cruz = "../Assets/Bigode.svg";
 
-function Menu_mobile(){
-    var menu = document.getElementById("dropside-mobile");
-    menu.classList.add("aberto");
-}
 
+btn_menu.addEventListener("click", function(){
+    
+    if (menu_lateral.classList.contains("aberto")) {
+        btn_menu.src = imagem_cruz;
+    } else {
+        btn_menu.src = imagem_nova;
+    }
+
+    menu_lateral.classList.toggle("aberto");
+    
+})
+
+
+let seta = document.getElementById("seta-servicos");
+let seta_sair = document.getElementById("seta-servicos_2");
+let menu_servicos = document.querySelector(".div-servicos_2")
+
+seta.addEventListener("click", function(){
+
+    menu_servicos.classList.toggle("aberto");
+    
+})
+
+seta_sair.addEventListener("click", function(){
+
+    menu_servicos.classList.remove("aberto");
+    
+})
 // let btn_menu =  document.querySelector('.btn-menu')
 // let menu_dropdown = document.getElementsByClassName('dropside-mobile')
 
