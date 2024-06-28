62aa9d708f020890f16e6531ab217d321b365b4e
PedroMajewski <pedrohmssilva@gmail.com>
2024-06-27 21:03:08 -0300

    commit-icone-header

--- ProjetoMensal/Home/home.js
+++ ProjetoMensal/Home/home.js
@@ -2,22 +2,25 @@
 let btn_menu = document.getElementById("btn-menu");
 let menu_lateral = document.querySelector(".dropside-mobile");
 
-let imagem_nova = "../Assets/Menu.svg";
-let imagem_cruz = "../Assets/Bigode.svg";
 
-
-btn_menu.addEventListener("click", function(){
-    
-    if (menu_lateral.classList.contains("aberto")) {
-        btn_menu.src = imagem_cruz;
-    } else {
-        btn_menu.src = imagem_nova;
-    }
-
+btn_menu.addEventListener("click",function(){
     menu_lateral.classList.toggle("aberto");
+    if(btn_menu.src.includes("menu.svg")) {
+        btn_menu.src = "../Assets/cross.svg";
+    } else {
+        btn_menu.src = "../Assets/menu.svg";
+    }
     
 })
 
+document.addEventListener("click", fecharMenu);
+
+function fecharMenu(event) {
+    if (!menu_lateral.contains(event.target) && !btn_menu.contains(event.target)) {
+        menu_lateral.classList.remove("aberto");
+        btn_menu.src = "../Assets/menu.svg";
+    }
+}
 
 let seta = document.getElementById("seta-servicos");
 let seta_sair = document.getElementById("seta-servicos_2");
